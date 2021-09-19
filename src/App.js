81723549705d94d7704934cdfe6ff37a6b462367
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-09-20 00:58:04 +0600

    React Animation:1.Using Transition Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import {Transition} from 'react-transition-group';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state={
+    show:true,
+  }
+  render() {
+    
+    return (
+      <div className="App">
+        
+        <h1>React Animation</h1>
+        <button onClick={()=>this.setState({show:!this.state.show})}>Toggle</button>
+
+        <Transition in={this.state.show} timeout={3000}>
+          {
+           mode=>(//<p>{mode}</p>
+        <div style={{
+            background:"black",
+            color:"white",
+            borderRadius:4,
+            opacity:mode==="exiting"?0:1,
+            transition:'opacity 2s ease-out',
+          }}>
+          <p>Hello Sajib!!</p>
+  
+        </div>
+        )
+          }
+        
+        </Transition>
+      </div>
+    );
+
+  }
+ 
 }
 
 export default App;
